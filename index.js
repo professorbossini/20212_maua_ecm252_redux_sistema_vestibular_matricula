f74f3274c5d256430620ce26ac953c1a4b5b842b
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-30 10:59:08 -0300

    Criacoes de ação e reducers implementados

---
+++ index.js
@@ -0,0 +1,42 @@
+const redux = require('redux')
+const prompts = require('prompts')
+
+//essa função é uma criadora de ação
+const realizarVestibular = (nome, cpf) => {
+    const entre6e10 = Math.random() <= 0.7
+    const nota = entre6e10 ? 6 + Math.random() * 4 : Math.random() * 5
+    //esse JSON que ela devolve é uma ação
+    return{
+        type: "REALIZAR_VESTIBULAR",
+        payload: {
+            nome, cpf, nota
+        }
+    }
+}
+
+//essa função também é uma criadora de ação
+const realizarMatricula = (cpf, status) => {
+    //esse JSON que ela devolve é uma ação
+    return {
+        type: "REALIZAR_MATRICULA",
+        payload:{
+            cpf, status
+        }
+    }
+}
+
+//essa função é um reducer
+const historicoVestibularReducer = (historicoVestibularAtual = [], acao) =>{
+    if (acao.type === "REALIZAR_VESTIBULAR"){
+        return [...historicoVestibularAtual, acao.payload]
+    }
+    return historicoVestibularAtual
+}
+
+//essa função é um reducer
+const historicoMatriculasReducer = (historicoMatriculasAtual = [], acao) =>{
+    if (acao.type === "REALIZAR_MATRICULA"){
+        return [...historicoMatriculasAtual, acao.payload]
+    }
+    return historicoMatriculasAtual
+}
